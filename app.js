037c6572f21591137a2d16f5e7b58cfd27671318
jianwenl <jianwenl@student.unimelb.edu.au>
2018-03-27 22:03:05 +1100

    a bit modified

--- app.js
+++ app.js
@@ -17,7 +17,5 @@ app.get('/bye', (req, res) => res.send("Good Night!"));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
-var server = app.listen(8088, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){ console.log(`Express listening on port ${PORT}`); });
