a31f93e03171a807b836fc66a9d76526eb3aeb00
reporkey <reporkey>
2018-04-08 12:43:04 +1000

    app

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const PORT = process.env.PORT || 3000;
-
 const express = require('express');
-var app = express();
-const router = require('./routers/router.js');
+const app = express();
 
-// set the view engine to ejs
+const router = require('./routers/router');
+
 app.set('view engine', 'ejs');
+app.use(router);
+app.use(express.static(__dirname +'/public'));
+app.use(function(req, res, next){
+    res.status(404);
 
-app.use('/', router);
-
-app.listen(PORT, function(){
-    console.log('Express serving at port ${PORT}');
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+    }
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){ });
+console.log(`Express listening on port ${PORT}`);
